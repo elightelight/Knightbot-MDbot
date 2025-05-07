9e57eb5d3f59aafd7523d665728daff8d2f3e521
elightelight <elightelight7@gmail.com>
2025-05-07 11:04:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "ELIGHT_ZW BOT",
   botOwner: 'ENGINEER ELIGHT', // Your name
   ownerNumber: '263711303785', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
